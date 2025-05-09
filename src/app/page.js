da3e345de3ecd0beb3bd467246fec3308c3eff5a
atul-xt <atul.xtt@gmail.com>
2025-05-09 09:57:55 +0530

    puja section added

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@ import VedicProfileForms from '@/components/VedicProfileForms';
 import LogoSlider from '@/components/LogoSlider';
 import AstrologyServices from '@/components/AstrologyServices';
 import HeroSection from '@/components/HeroSection';
+import EventCard from '@/components/EventCard';
 
 const images = [
   { alt: "Amazon", imageUrl: "https://astroarunpandit.org/wp-content/uploads/2024/08/Group-3131.png" },
@@ -42,9 +43,49 @@ const images2 = [
 
 export default function Home() {
 
+  const pujaDetails = [
+    {
+      id: 1,
+      title: "Hariyali Teej",
+      desc: "Tritiya, or the third day of Shukla Paksha of Shravan month, is termed Hariyali Teej or Shravani Teej. As per the English calendar, Hariyali Teej is often in July or August. It is primarily a festival of women.",
+      imageUrl: "https://content.vinaybajrangi.com/img/content/50_Hariyali%20Teej.webp?v=1",
+      date: "Sunday, January 01, 2023 • 06:00 PM",
+      highlight: "Special Puja",
+      address: "Near Greater Noida",
+      by: "Vinay Bajrangi"
+    },
+    {
+      id: 2,
+      title: "Kajari Teej",
+      desc: "Tritiya, or the third day of Shukla Paksha of Shravan month, is termed Hariyali Teej or Shravani Teej. As per the English calendar, Hariyali Teej is often in July or August. It is primarily a festival of women.",
+      imageUrl: "https://www.vinaybajrangi.com/_next/image?url=https%3A%2F%2Fcontent.vinaybajrangi.com%2Fimg%2Fcontent%2F1729162834-kajari-teej.jpg&w=1200&q=75",
+      date: "Monday, March 06, 2024 • 07:00 AM",
+      highlight: "Special Puja",
+      address: "Near Greater Noida",
+      by: "Vinay Bajrangi"
+    },
+    {
+      id: 3,
+      title: "Nag Panchami",
+      desc: "Tritiya, or the third day of Shukla Paksha of Shravan month, is termed Hariyali Teej or Shravani Teej. As per the English calendar, Hariyali Teej is often in July or August. It is primarily a festival of women.",
+      imageUrl: "https://www.vinaybajrangi.com/_next/image?url=https%3A%2F%2Fcontent.vinaybajrangi.com%2Fimg%2Fcontent%2F1723116937-nag-panchami.jpg&w=1200&q=75",
+      date: "Friday, March 23, 2025 • 08:00 PM",
+      highlight: "Special Puja",
+      address: "Near Greater Noida",
+      by: "Vinay Bajrangi"
+    },
+  ]
+
   return (
     <main>
-      <HeroSection />
+      {/* <HeroSection /> */}
+      <section className='py-10'>
+        <div className="mb-8 text-center">
+          <h1 className="text-red-900 text-3xl font-semibold">Special Pujas for Peace, Prosperity & Protection</h1>
+          <p className="mt-1 max-w-2xl mx-auto ">Explore our handpicked selection of powerful Vedic Pujas performed by expert pandits. Whether it's for health, success, marriage, or removing obstacles – find the perfect puja to align your life with divine blessings.</p>
+        </div>
+        <EventCard data={pujaDetails} />
+      </section>
       <LifeProblemsSection />
       <LogoSlider images={images} />
       <LogoSlider images={images2} isReverse={true} />
---
+++ src/components/Loader.js
@@ -0,0 +1,55 @@
+export default function Loader() {
+    return (
+        <div className="fixed inset-0 bg-white z-50">
+            <div className="py-5 w-full bg-gray-300 animate-pulse rounded-2xl" />
+            <div className="max-w-7xl mx-auto w-full py-4 flex justify-between items-center gap-10">
+                <div className="flex justify-between items-center w-1/2">
+                    <div className="h-16 w-16 rounded-full bg-gray-300 animate-pulse"></div>
+                    <div className="py-5 w-52 bg-gray-300 animate-pulse rounded-2xl"></div>
+                </div>
+                <div className="flex justify-between w-1/2">
+                    <div className="flex gap-3">
+                        <div className="h-10 w-20 bg-gray-300 animate-pulse rounded-2xl"></div>
+                        <div className="h-10 w-20 bg-gray-300 animate-pulse rounded-2xl"></div>
+                    </div>
+                    <div className="flex gap-3">
+                        <div className="h-10 w-10 bg-gray-300 animate-pulse rounded-2xl"></div>
+                        <div className="h-10 w-10 bg-gray-300 animate-pulse rounded-2xl"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-3 max-w-7xl mx-auto flex gap-3 items-center">
+                <span className="py-3 w-16 bg-gray-300 animate-pulse rounded-2xl" />
+                <span className="py-3 w-32 bg-gray-300 animate-pulse rounded-2xl" />
+                <span className="py-3 w-32 bg-gray-300 animate-pulse rounded-2xl" />
+                <span className="py-3 w-28 bg-gray-300 animate-pulse rounded-2xl" />
+                <span className="py-3 w-28 bg-gray-300 animate-pulse rounded-2xl" />
+                <span className="py-3 w-26 bg-gray-300 animate-pulse rounded-2xl" />
+                <span className="py-3 w-26 bg-gray-300 animate-pulse rounded-2xl" />
+                <span className="py-3 w-24 bg-gray-300 animate-pulse rounded-2xl" />
+                <span className="py-3 w-24 bg-gray-300 animate-pulse rounded-2xl" />
+                <span className="py-3 w-24 bg-gray-300 animate-pulse rounded-2xl" />
+                <span className="py-3 w-24 bg-gray-300 animate-pulse rounded-2xl" />
+                <span className="py-3 w-24 bg-gray-300 animate-pulse rounded-2xl" />
+            </div>
+            <div className="mt-5 max-w-[1300px] mx-auto h-12 bg-gray-300 animate-pulse rounded-xl"></div>
+            <div className="mt-5 py-40 w-full flex items-center justify-center bg-gray-300 animate-pulse">
+                <div className="max-w-6xl mx-auto flex justify-between w-full">
+                    <div className="">
+                        <div className="space-y-3">
+                            <div className="py-6 min-w-md bg-gray-200 animate-pulse rounded-2xl" />
+                            <div className="py-6 w-80 bg-gray-200 animate-pulse rounded-2xl" />
+                            <div className="py-6 w-96 bg-gray-200 animate-pulse rounded-2xl" />
+                        </div>
+                        <div className="mt-8">
+                            <div className="py-3 w-[20rem] bg-gray-200 animate-pulse rounded-2xl"></div>
+                        </div>
+                    </div>
+                    <div className="h-72 w-72 bg-gray-200 animate-pulse rounded-full">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
